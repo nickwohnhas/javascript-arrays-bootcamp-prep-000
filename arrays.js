6bc2e85586028fb5fc19d8eb44eecf0f9155eccb
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-04-07 17:18:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,5 +27,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
   return array
 }
 function removeElementFromBeginningOfArray(array){
-  
+  newarray = array.slice(1)
+  return newarray
 }
